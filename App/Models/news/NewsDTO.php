64ac2800b67287fd113c23f30d23ea923d3eb3f5
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-09-11 23:24:24 +0300

    Created News DTO

---
+++ App/Models/news/NewsDTO.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Models\news;
+
+class NewsDTO
+{
+    private int $id;
+    private string $title;
+    private string $description;
+    private mixed $date;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDate(): mixed
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param mixed $date
+     */
+    public function setDate(mixed $date): void
+    {
+        $this->date = $date;
+    }
+
+
+}
