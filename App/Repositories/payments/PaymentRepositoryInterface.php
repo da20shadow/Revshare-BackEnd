663fc8876479ffed6f6eeb3e4b667e091b4f1878
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-09-11 23:26:51 +0300

    Updated payments repository interface

--- App/Repositories/payments/PaymentRepositoryInterface.php
+++ App/Repositories/payments/PaymentRepositoryInterface.php
@@ -4,12 +4,12 @@ namespace App\Repositories\payments;
 
 interface PaymentRepositoryInterface
 {
-    public function insertDeposit($amount,$user_id);
-    public function insertWithdrawal($amount,$user_id);
+    public function insertDeposit($amount,$coins,$wallet,$processor,$user_id);
+    public function insertWithdrawal($amount,$processor,$user_wallet,$user_id);
     public function updateWithdrawalRequestStatus($status,$request_id);
-    public function updateDepositStatus($status,$deposit_id);
+    public function updateDepositStatus($amount,$deposit_id,$user_id);
     public function deleteRequest($request_id,$user_id);
     public function getPaymentProcessors();
-    public function getDepositHistoryByUserId($user_id);
-    public function getWithdrawalHistoryByUserId($user_id);
+    public function getDepositHistoryByUserId($offset,$user_id);
+    public function getWithdrawalHistoryByUserId($offset,$user_id);
 }
