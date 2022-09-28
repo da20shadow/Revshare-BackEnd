e200ddccfbcaa2e783f377ee5cd8ddf8b69c4da4
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-09-28 22:20:49 +0300

    Added share dividends method

--- App/Repositories/shares/SharesRepository.php
+++ App/Repositories/shares/SharesRepository.php
@@ -2,7 +2,11 @@
 
 namespace App\Repositories\shares;
 
+use App\Models\shares\DividendsDTO;
 use App\Models\shares\SharesDTO;
+use App\Models\shares\SharesStatDTO;
+use App\Models\user\UserDTO;
+use App\Models\user\UserStat;
 use Database\DBConnector;
 use Database\PDODatabase;
 
@@ -15,6 +19,7 @@ class SharesRepository implements SharesRepositoryInterface
         $this->db = DBConnector::create();
     }
 
+    /** --------------------- POST --------------------- */
     public function insert($quantity, $price, $userId): array|string
     {
         try {
@@ -41,7 +46,7 @@ class SharesRepository implements SharesRepositoryInterface
         ];
     }
 
-    public function buy($orderOwnerId,$referId,$quantity, $costs, $orderId, $userId, $buyAll): bool
+    public function buy($orderOwnerId, $referId, $quantity, $costs, $orderId, $userId, $buyAll): bool
     {
         $profitForOwner = $costs;
         $profitForRefer = $costs * 0.1;
@@ -69,9 +74,9 @@ class SharesRepository implements SharesRepositoryInterface
                     ':orderOwnerId' => $orderOwnerId,
                 ));
                 return true;
-            }catch (\PDOException $exception){
+            } catch (\PDOException $exception) {
                 echo json_encode([
-                    'message'=>$exception->getMessage()
+                    'message' => $exception->getMessage()
                 ]);
                 return false;
             }
@@ -88,27 +93,66 @@ class SharesRepository implements SharesRepositoryInterface
                 UPDATE marketplace SET quantity = quantity - :quantity
                 WHERE order_id = :order_id;
                 ")->execute(array(
-                    ':quantity' => $quantity,
-                    ':costs' => $costs,
-                    ':user_id' => $userId,
-                    ':order_id' => $orderId,
-                    ':profitForOwner' => $profitForOwner,
-                    ':profitForRefer' => $profitForRefer,
-                    ':referId' => $referId,
-                    ':orderOwnerId' => $orderOwnerId,
+                ':quantity' => $quantity,
+                ':costs' => $costs,
+                ':user_id' => $userId,
+                ':order_id' => $orderId,
+                ':profitForOwner' => $profitForOwner,
+                ':profitForRefer' => $profitForRefer,
+                ':referId' => $referId,
+                ':orderOwnerId' => $orderOwnerId,
             ));
             return true;
-        }catch (\PDOException $exception){
+        } catch (\PDOException $exception) {
             echo json_encode([
-                'message'=>$exception->getMessage()
+                'message' => $exception->getMessage()
             ]);
             return false;
         }
     }
 
+    /** Share dividends */
+    public function shareDividends($profitToAdd,$user_id): bool
+    {
+        try {
+            $this->db->query("
+                UPDATE users SET balance = balance + :profit 
+                WHERE user_id = :user_id
+            ")->execute(array(
+                ':profit' => $profitToAdd,
+                'user_id' => $user_id
+            ));
+            return true;
+        }catch (\PDOException $exception) {
+            echo json_encode([
+                'Error' => $exception->getMessage()
+            ]);
+            return false;
+        }
+    }
+
+    /** Save shared dividends history */
+    public function saveDividendHistory($sharedAmount,$percentReturn): bool
+    {
+        try {
+            $this->db->query("
+                INSERT INTO dividends_history (amount, percent_return) 
+                VALUES (:amount,:percent_return)
+            ")->execute(array(
+                ':amount' => $sharedAmount,
+                ':percent_return' => $percentReturn
+            ));
+            return true;
+        }catch (\PDOException $exception){
+            echo json_encode([
+                'Error' => $exception->getMessage()
+            ]);
+            return false;
+        }
+    }
 
     /** UPDATE */
-    public function update($price,$order_id, $userId): bool
+    public function update($price, $order_id, $userId): bool
     {
         try {
             $this->db->query("
@@ -121,20 +165,20 @@ class SharesRepository implements SharesRepositoryInterface
                 ':user_id' => $userId
             ));
             return true;
-        }catch (\PDOException $exception){
+        } catch (\PDOException $exception) {
             return false;
         }
     }
 
     /** ----------------DELETE---------------- */
-    public function delete($orderId, $userId,$sharesInOrder): bool
+    public function delete($orderId, $userId, $sharesInOrder): bool
     {
         try {
             $this->db->query("
                 DELETE FROM marketplace
                 WHERE order_id = :order_id AND user_id = :user_id;
                 UPDATE users 
-                SET balance = balance - 1, shares = shares + :shares 
+                SET balance = balance - 3, shares = shares + :shares 
                 WHERE user_id = :user_id;
             ")->execute(array(
                 'order_id' => $orderId,
@@ -142,7 +186,7 @@ class SharesRepository implements SharesRepositoryInterface
                 'shares' => $sharesInOrder
             ));
             return true;
-        }catch (\PDOException $exception){
+        } catch (\PDOException $exception) {
             return false;
         }
     }
@@ -167,6 +211,42 @@ class SharesRepository implements SharesRepositoryInterface
         return $shares;
     }
 
+    /** Get dividends history */
+    public function getDividendsHistory(): ?\Generator
+    {
+        $dividendsHistory = null;
+        try {
+            $dividendsHistory = $this->db->query("
+                SELECT week_id AS id,
+                       amount,
+                       percent_return AS percent, 
+                       date 
+                FROM dividends_history 
+                ORDER BY date DESC LIMIT 48
+            ")->execute()->fetch(DividendsDTO::class);
+        }catch (\PDOException $exception){
+            echo json_encode([
+                'Error' => $exception->getMessage()
+            ]);
+        }
+        return $dividendsHistory;
+    }
+    /** Get total profit paid */
+    public function getProfitPaid()
+    {
+        $profitPaid = null;
+        try {
+            $profitPaid = $this->db->query("
+                SELECT SUM(amount) as total FROM dividends_history;
+            ")->execute()->fetch(DividendsDTO::class)->current();
+        }catch (\PDOException $exception){
+            echo json_encode([
+                'Error' => $exception->getMessage()
+            ]);
+        }
+        return $profitPaid;
+    }
+
     public function getOrderId($orderId)
     {
         $order = null;
@@ -178,7 +258,7 @@ class SharesRepository implements SharesRepositoryInterface
             ")->execute(array(
                 ':order_id' => $orderId
             ))->fetch(SharesDTO::class)->current();
-        }catch (\PDOException $exception){
+        } catch (\PDOException $exception) {
             //TODO process the errors
         }
         return $order;
@@ -201,4 +281,76 @@ class SharesRepository implements SharesRepositoryInterface
         return $orders;
     }
 
+    /** Get shareholders */
+    public function getShareholders(): ?\Generator
+    {
+        $shareholders = null;
+        try {
+            $shareholders = $this->db->query("
+                SELECT user_id AS id, shares 
+                FROM users 
+                WHERE shares >= 1
+            ")->execute()
+                ->fetch(UserStat::class);
+        } catch (\PDOException $exception) {
+            echo json_encode(['Error' => $exception->getMessage()]);
+        }
+        return $shareholders;
+    }
+
+    /** GET total shares */
+    public function getTotalShares()
+    {
+        $total = null;
+        try {
+            $total = $this->db->query("
+            Select sum(columnToSum) as total
+            From(
+            SELECT shares as columnToSum FROM users
+            Union all
+            SELECT quantity as columnToSum FROM marketplace
+            )as nestedQuery
+        ")->execute()->fetch(SharesStatDTO::class)->current();
+        } catch (\PDOException $exception) {
+            echo json_encode([
+                'Error' => $exception->getMessage()
+            ]);
+        }
+
+        return $total;
+    }
+
+    /** GET total shares in users accounts */
+    public function getTotalHoldShares()
+    {
+        $totalHold = null;
+        try {
+            $totalHold = $this->db->query("
+            SELECT sum(shares) as hold
+            FROM users
+        ")->execute()->fetch(SharesStatDTO::class)->current();
+        } catch (\PDOException $exception) {
+            echo json_encode([
+                'Error' => $exception->getMessage()
+            ]);
+        }
+        return $totalHold;
+    }
+
+    /** GET total shares in marketplace */
+    public function getTotalOrderShares()
+    {
+        $totalOrders = null;
+        try {
+            $totalOrders = $this->db->query("
+            SELECT sum(quantity) as orders
+            FROM marketplace
+        ")->execute()->fetch(SharesStatDTO::class)->current();
+        } catch (\PDOException $exception) {
+            echo json_encode([
+                'Error' => $exception->getMessage()
+            ]);
+        }
+        return $totalOrders;
+    }
 }
