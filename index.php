22535068c1d64c9b87e58c9d1a6a0ed2b29f8d9e
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-09-28 22:21:34 +0300

    Added some api catches

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Services\news\NewsService;
+use App\Services\payments\PaymentService;
 use App\Services\shares\SharesService;
 use App\Services\user\UserService;
 
@@ -44,7 +46,7 @@ if (preg_match("/^\/users[\/]?$/", $url))
     /** UPDATE user */
     elseif ($_SERVER['REQUEST_METHOD'] === 'PATCH' && isset($inputData))
     {   //TODO
-        $apiHandler->processUserPATCHRequest($inputData,$userService);
+        $apiHandler->processUserPATCHRequest($inputData,$token,$userService);
     }
     /** GET user */
     elseif ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($token))
@@ -64,7 +66,29 @@ else if(preg_match("/^\/users\/referrals\/?$/",$url)){
         $apiHandler->processGETUserReferralsRequest($token,$userService);
     }
 }
-
+/** ----------------------SHARES API Requests---------------------- */
+else if (preg_match("/^\/shares\/stat[\/]?$/", $url))
+{
+    $sharesService = new SharesService();
+    /** get shares stat */
+    if ($_SERVER['REQUEST_METHOD'] === 'GET')
+    {
+        $apiHandler->processGetSharesStat($sharesService);
+    }
+}
+else if (preg_match("/^\/share\/dividends[\/]?$/",$url))
+{
+    $sharesService = new SharesService();
+    /** get dividends history */
+    if ($_SERVER['REQUEST_METHOD'] === 'GET')
+    {
+        $apiHandler->processGetSharedDividends($sharesService);
+    }
+    else if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($inputData))
+    {
+        $apiHandler->processShareDividendsRequest($token,$inputData,$sharesService);
+    }
+}
 else if (preg_match("/^\/shares[\/]?$/", $url))
 {
     $sharesService = new SharesService();
@@ -77,17 +101,17 @@ else if (preg_match("/^\/shares[\/]?$/", $url))
     /** UPDATE order */
     elseif ($_SERVER['REQUEST_METHOD'] === 'PATCH' && isset($inputData))
     {   //TODO
-        $apiHandler->processSharesPATCHRequest($inputData,$sharesService);
+        $apiHandler->processSharesPATCHRequest($inputData,$token,$sharesService);
     }
-    /** GET user */
+    /** GET Order */
     elseif ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($token))
     {   //TODO return account stat
         $apiHandler->processSharesGETRequest($token,$sharesService);
     }
-    /** DELETE User */
+    /** DELETE Order */
     elseif ($_SERVER['REQUEST_METHOD'] === 'DELETE' && isset($inputData))
     {   //TODO
-        $apiHandler->processSharesDELETERequest($inputData,$sharesService);
+        $apiHandler->processSharesDELETERequest($inputData,$token,$sharesService);
     }
 }
 else if (preg_match("/^\/shares\/\d+$/", $url))
@@ -120,7 +144,111 @@ else if (preg_match("/^\/user\/shares\/$/",$url)){
     $sharesService = new SharesService();
     $apiHandler->processGetOrdersByUserId($token,$sharesService);
 }
+/** ------------------PAYMENTS API Requests------------------ */
+else if (preg_match("/^\/payment[\/]?$/",$url))
+{
+    $paymentsService = new PaymentService();
 
+    if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($token))
+    {
+        $apiHandler->processPaymentGETRequest($token,$paymentsService);
+    }
+    /** Withdrawal request */
+    else if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($inputData) && isset($token))
+    {
+        $apiHandler->processWithdrawalRequest($token,$inputData,$paymentsService);
+    }
+
+}
+else if (preg_match("/^\/deposit[\/]?$/",$url))
+{
+    $paymentsService = new PaymentService();
+
+    /** Deposit request */
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($inputData) && isset($token))
+    {
+        $apiHandler->processDepositRequest($token,$inputData,$paymentsService);
+    }
+    else if ($_SERVER['REQUEST_METHOD'] === 'PATCH' && isset($inputData) && isset($token))
+    {
+        $apiHandler->processUpdateDepositRequest($token,$inputData,$paymentsService);
+    }
+
+}
+else if (preg_match("/^\/deposit\/\d+$/",$url))
+{
+    $offset = str_replace('/deposit/','',$url);
+    $paymentsService = new PaymentService();
+    /** Get deposit history */
+    if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($token))
+    {
+        $apiHandler->processGetDepositHistory($token,$offset,$paymentsService);
+    }
+    else if ($_SERVER['REQUEST_METHOD'] === 'DELETE' && isset($token))
+    {
+        $apiHandler->processDELETEDepositRequest($offset,$token,$paymentsService);
+    }
+
+}
+else if (preg_match("/^\/pending-deposits\/\d+$/",$url))
+{
+    $offset = str_replace('/pending-deposits/','',$url);
+    $paymentsService = new PaymentService();
+    /** Get deposit history */
+    if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($token))
+    {
+        $apiHandler->processGetPendingDeposits($token,$offset,$paymentsService);
+    }
+
+}
+else if (preg_match("/^\/pending-withdrawals\/\d+$/",$url))
+{
+    $offset = str_replace('/pending-withdrawals/','',$url);
+    $paymentsService = new PaymentService();
+    /** Get deposit history */
+    if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($token))
+    {
+        $apiHandler->processGetPendingWithdrawals($token,$offset,$paymentsService);
+    }
+
+}
+else if (preg_match("/^\/withdrawal\/\d+$/",$url))
+{
+    $offset = str_replace('/withdrawal/','',$url);
+    $paymentsService = new PaymentService();
+    /** Get withdrawals history */
+    if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($token))
+    {
+        $apiHandler->processGetWithdrawalHistory($token,$offset,$paymentsService);
+    }
+
+}
+else if (preg_match("/^\/wallets[\/]?$/",$url))
+{
+    $paymentsService = new PaymentService();
+    /** GET user wallets */
+    if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($token))
+    {
+        $apiHandler->processGetUserWallets($token,$paymentsService);
+    }
+    else if ($_SERVER['REQUEST_METHOD'] === 'PATCH' && isset($inputData) && isset($token))
+    {
+        $apiHandler->processUpdateWallets($token,$inputData,$paymentsService);
+    }
+}
+else if (preg_match("/^\/news[\/]?$/",$url))
+{
+    $newsService = new NewsService();
+    /** GET News */
+    if ($_SERVER['REQUEST_METHOD'] === 'GET')
+    {
+        $apiHandler->processGetNews($newsService);
+    }
+    else if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($inputData) && isset($token))
+    {
+        $apiHandler->processPublishNews($token,$inputData,$newsService);
+    }
+}
 else{
     http_response_code(404);
     echo json_encode(['message' => 'Error! Invalid API Request!']);
